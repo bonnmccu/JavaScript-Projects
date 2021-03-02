f81cafed5e0a0f3cf3d8166afa91219b971fee71
Bonnie Mack <78276424+bonnmccu@users.noreply.github.com>
2021-03-02 11:03:38 -0700

    Add files via upload
    Assignment JS Course Step 57

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+window.alert ("This is my assignment JS Step");  //This is my alert window 
+
+var B = "concatenated" + " string" //Linking a string example
+document.write(B)
+
+document.write(17+80)//an expression
+
+
+
+var dogs = "Breeds", Ollie ="Silkie", Rosey = "Poodle",Roxie ="Poshie", Dudley = "Cheweeny", Genie = "Collie"; //variable dogs assgined -instead of family names I used pet breeds.
+var dogs = dogs.fontcolor("purple"); //variable dogs assigned the color purple
+document.write(dogs); //print var
+
+
+var envy = "Envy is the green-eyed monster."; //variable envy assigned
+var envy = envy.fontcolor ("green"); //var envy assigned color green
+document.write (envy); //print var
+
+
+
+
+
+
+
+    
